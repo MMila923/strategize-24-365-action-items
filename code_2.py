6a9a40aac837c0bb50700c37d959d35bfb8c5654
Mila <m9234434@gmail.com>
2023-11-10 07:31:07 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,40 @@
+def generate_random_data():
+import random
+
+
+if __name__ == "__main__":
+    main()
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    return data
+
+    return data
+    main()
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    for item in data:
+    data = generate_random_data()
+def main():
+
+import random
+
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+if __name__ == "__main__":
+
+    data = generate_random_data()
+    return data
+    main()
+    for item in data:
+
+import random
