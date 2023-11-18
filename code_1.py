f4af4a9e767b4d429a74ed2251164c86dbcc5cbf
Mila <m9234434@gmail.com>
2023-11-18 17:08:43 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,16 +1,39 @@
-function main() {
-}
-    return data;
-    data.forEach(item => {
-    });
-}
-    }
-        console.log("Random Number: " + item);
 
-function generateRandomData() {
-main();
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
+import random
+def generate_random_data():
 
-        data.push(Math.floor(Math.random() * 100) + 1);
+
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def main():
+        print(f"Random Number: {item}")
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+    return data
+
+def generate_random_data():
+def main():
+    for item in data:
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+
+    data = generate_random_data()    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
+    return data
+
+    for item in data:
+        print(f"Random Number: {item}")
+
+
+def generate_random_data():
+if __name__ == "__main__":
+import random
